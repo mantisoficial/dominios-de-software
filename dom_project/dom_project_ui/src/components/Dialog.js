d80e0dcf116a9c8e52b9bd4e8af46da00aa5e9bf
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-03-15 14:21:59 -0300

    Criação do formulário para questões.

--- dom_project/dom_project_ui/src/components/Dialog.js
+++ dom_project/dom_project_ui/src/components/Dialog.js
@@ -1,12 +1,121 @@
 import React from "react";
 import "./Dialog.css";
+import Button from "./Button";
 
-const Dialog = ({ isOpen }) => {
+const Dialog = ({
+  name,
+  isOpen,
+  placeholder,
+  value,
+  onChange,
+  onCancel,
+  onSave,
+}) => {
+  const subjectOptions = [
+    {
+      name: "Matemática",
+      value: "A",
+    },
+    {
+      name: "História",
+      value: "B",
+    },
+    {
+      name: "Física",
+      value: "C",
+    },
+    {
+      name: "Geografia",
+      value: "D",
+    },
+  ];
+
   return isOpen ? (
     <div className="dialog-popup">
       <div className="dialog-content">
         <div className="dialog-header">
-          <p>Teste</p>
+          <p className="subheader">Nova questão</p>
+        </div>
+        <div className="dialog-body">
+          <div className="input-row">
+            <div style={{ margin: "auto 0px" }}>
+              <p className="body" style={{ margin: "0px 8px 0px 0px" }}>
+                {" "}
+                Código:{" "}
+              </p>
+            </div>
+            <input
+              name={name}
+              className="text-input"
+              type="text"
+              placeholder={placeholder ?? null}
+              onChange={onChange}
+              value={value}
+              autoComplete="off"
+              maxLength={4}
+              style={{ width: "40px" }}
+            ></input>
+          </div>
+          <div className="input-row" style={{ display: "block" }}>
+            <div style={{ margin: "auto 0px" }}>
+              <p className="body" style={{ margin: "0px 1px 4px 0px" }}>
+                {" "}
+                Questão:{" "}
+              </p>
+            </div>
+            <input
+              name={name}
+              className="text-input"
+              type="text"
+              placeholder={placeholder ?? null}
+              onChange={onChange}
+              value={value}
+              autoComplete="off"
+              style={{ width: "250px", height: "30px" }}
+            ></input>
+          </div>
+          <div className="input-row">
+            <div style={{ margin: "auto 0px" }}>
+              <p className="body" style={{ margin: "0px 1px 0px 0px" }}>
+                {" "}
+                Matéria:{" "}
+              </p>
+            </div>
+            <select
+              name={name}
+              className="text-input"
+              placeholder={placeholder ?? null}
+              onChange={onChange}
+              value={value}
+            >
+              {subjectOptions.map((item, index) => {
+                return <option value={item.value}>{item.name}</option>;
+              })}
+            </select>
+          </div>
+          <div className="input-row">
+            <div style={{ margin: "auto 0px" }}>
+              <p className="body" style={{ margin: "0px 1px 0px 0px" }}>
+                {" "}
+                Revisado:{" "}
+              </p>
+            </div>
+            <input
+              name={name}
+              className="text-input"
+              type="checkbox"
+              onChange={onChange}
+              value={value}
+            ></input>
+          </div>
+        </div>
+        <div className="dialog-footer">
+          <Button
+            label="Salvar"
+            onClick={onSave}
+            buttonMargin={"0px 8px 0px 0px"}
+          />
+          <Button label="Cancelar" isDestructive onClick={onCancel} />
         </div>
       </div>
     </div>
