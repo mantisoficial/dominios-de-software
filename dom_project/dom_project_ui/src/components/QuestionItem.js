21034533eef65bdd25d9fd135d2ffb8f8a99a8de
pokz <pietronieroboss@gmail.com>
2022-03-14 22:08:15 -0300

    Melhorias e adequações no componente QuestionItem.

--- dom_project/dom_project_ui/src/components/QuestionItem.js
+++ dom_project/dom_project_ui/src/components/QuestionItem.js
@@ -1,27 +1,43 @@
 import React from "react";
 import "./QuestionItem.css";
+import SubjectBadge from "./SubjectBadge";
 
 const QuestionItem = ({ item }) => {
   return (
     <div className="item-container">
       <div className="item-content-header">
-        <p>{item.code}</p>
-        <p>{item.name}</p>
+        <p
+          className="subheader"
+          style={{ fontWeight: "550", marginRight: "4px" }}
+        >
+          {item.code})
+        </p>
+        <p className="subheader">{item.name}</p>
       </div>
       <div className="item-content-body">
-        <p>{item.question}</p>
+        <p className="body" style={{ margin: "0px 0px 8px 0px" }}>
+          {item.question}
+        </p>
         {item.subquestions.length > 0
           ? item.subquestions.map((item, index) => {
               return (
-                <div key={index}>
-                  <p>{item.subquestion}</p>
+                <div key={index} style={{ margin: "0px 0px 8px 0px" }}>
+                  <p className="metadata">{item.text}</p>
                 </div>
               );
             })
           : null}
       </div>
       <div className="item-content-footer">
-        <p>Matéria: {item.subject}</p>
+        <div style={{ margin: "auto 0px" }}>
+          <p className="metadata" style={{ marginRight: "4px" }}>
+            Matéria:{" "}
+          </p>
+        </div>
+        <SubjectBadge
+          isExactScience={item.subjectType === "Exact" ? true : false}
+          label={item.subject}
+        />
       </div>
     </div>
   );
