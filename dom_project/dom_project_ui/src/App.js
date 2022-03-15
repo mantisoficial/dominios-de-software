4fe9cf1779049d9e1bdf6b1f2ed1b323bd733938
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-03-15 16:46:58 -0300

    Retirada de Subquestions. Dialog implementada com novas propriedades.

--- dom_project/dom_project_ui/src/App.js
+++ dom_project/dom_project_ui/src/App.js
@@ -10,14 +10,6 @@ function App() {
       code: 2605,
       name: "Teste",
       question: "Qual o nome do stone?",
-      subquestions: [
-        {
-          text: "a) Subitem 1",
-        },
-        {
-          text: "b) Subitem 2",
-        },
-      ],
       subject: "História",
       subjectType: "Human",
       reviewed: true,
@@ -26,12 +18,12 @@ function App() {
       code: 2812,
       name: "Teste 2",
       question: "Qual a idade do stone?",
-      subquestions: [],
       subject: "Matemática",
       subjectType: "Exact",
       reviewed: false,
     },
   ]);
+
   //properties
   //code, name, question, subquestion (optional), subject, reviewed
 
@@ -49,9 +41,14 @@ function App() {
         </p>
       </header>
       <div className="action-buttons">
-        <Button label="Excluir" isDestructive buttonMargin="0px 16px 0px 0px" />
         <Button label="Adicionar" onClick={toggleModal} />
-        <Dialog isOpen={isModalOpen} />
+        {isModalOpen ? (
+          <Dialog
+            isOpen={isModalOpen}
+            onCancel={toggleModal}
+            existingQuestions={questions}
+          />
+        ) : null}
       </div>
       <div className="questions-list">
         {questions.map((item, index) => {
