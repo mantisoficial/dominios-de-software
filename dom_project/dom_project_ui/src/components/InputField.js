5ea4b5bd8b43697baf8ea573a8f7cdab714eae98
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-03-15 14:46:01 -0300

    Componente InutField criado.

---
+++ dom_project/dom_project_ui/src/components/InputField.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./InputField.css";
+
+const InputField = ({ name, onChange, value, placeholder, inputStyle, inputLength}) => {
+  return (
+    <input
+      name={name}
+      className="text-input"
+      type="text"
+      placeholder={placeholder ?? null}
+      onChange={onChange}
+      value={value}
+      style={inputStyle}
+      maxLength={inputLength}
+    ></input>
+  );
+};
+
+export default InputField;
