85e9e66eaec3127dafc5f462c429cacb3e359840
pokz <pietronieroboss@gmail.com>
2022-03-14 21:38:48 -0300

    Criação do json de objetos. Implementação do componente QuestionItem.

--- dom_project/dom_project_ui/src/App.js
+++ dom_project/dom_project_ui/src/App.js
@@ -1,22 +1,49 @@
 import "./App.css";
 import Button from "./components/Button";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import QuestionItem from "./components/QuestionItem";
 
 function App() {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([
+    {
+      code: 2605,
+      name: "Teste",
+      question: "Qual o nome do stone?",
+      subquestions: [],
+      subject: "História",
+      reviewed: true,
+    },
+    {
+      code: 2812,
+      name: "Teste 2",
+      question: "Qual a idade do stone?",
+      subquestions: [],
+      subject: "Matemática",
+      reviewed: false,
+    },
+  ]);
   //properties
   //code, name, question, subquestion (optional), subject, reviewed
 
   return (
     <div className="app">
       <header className="app-header">
-        <p className="header" style={{color: "white"}}>Identificador único de questões</p>
+        <p className="header" style={{ color: "white" }}>
+          Identificador único de questões
+        </p>
       </header>
       <div className="action-buttons">
         <Button label="Excluir" isDestructive buttonMargin="0px 16px 0px 0px" />
         <Button label="Adicionar" />
       </div>
       <div className="questions-list">
+        {questions.map((item, index) => {
+          return (
+            <div key={index}>
+              <QuestionItem item={item} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
