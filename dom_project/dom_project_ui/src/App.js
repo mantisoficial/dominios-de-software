5ae70fda00d712c24be94c9611aac162d1fa1edf
pokz <pietronieroboss@gmail.com>
2022-03-14 21:20:26 -0300

    Implementação dos botões na View principal.

--- dom_project/dom_project_ui/src/App.js
+++ dom_project/dom_project_ui/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import "./App.css";
+import Button from "./components/Button";
+import React, {useState} from "react";
 
 function App() {
+  const [questions, setQuestions] = useState([])
+  //properties
+  //code, name, question, subquestion (optional), subject, reviewed
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Identificador único de questões
-        </p>
+    <div className="app">
+      <header className="app-header">
+        <p className="header" style={{color: "white"}}>Identificador único de questões</p>
       </header>
+      <div className="action-buttons">
+        <Button label="Excluir" isDestructive buttonMargin="0px 16px 0px 0px" />
+        <Button label="Adicionar" />
+      </div>
+      <div className="questions-list">
+      </div>
     </div>
   );
 }
