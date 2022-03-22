7d8ff3f8e758013771ca4f177f1a7765da7c4e03
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-03-22 16:12:42 -0300

    Página de login adicionada nas rotas.

--- dom_project/dom_project_ui/src/index.js
+++ dom_project/dom_project_ui/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./LoginPage";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/app" element={<App />} />
       </Routes>
     </BrowserRouter>
