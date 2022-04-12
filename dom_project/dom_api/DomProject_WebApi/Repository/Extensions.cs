e029d26a1570e890fb189a5cbb4d9dc8144fe003
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-12 12:20:15 -0300

    Criação do repositório base e context para conexão com o BD.

---
+++ dom_project/dom_api/DomProject_WebApi/Repository/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using DomProject_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomProject_WebApi.Repository
+{
+    internal static class Extensions
+    {
+        public static void SetTraceValues(this Traceable traceable)
+        {
+            traceable.LastWriteDate = DateTime.Now;
+
+            if (!traceable.CreationDate.HasValue)
+                traceable.CreationDate = traceable.LastWriteDate;
+        }
+
+        public static void SetUpdateValues(this Traceable traceable)
+        {
+            traceable.LastWriteDate = DateTime.Now;
+        }
+    }
+}
---
+++ dom_project/dom_api/DomProject_WebApi/Repository/Sql/DomProjectContext.cs
@@ -0,0 +1,29 @@
+﻿using DomProject_WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomProject_WebApi.Repository.Sql
+{
+    public class DomProjectContext : DbContext
+    {
+        public DomProjectContext() { }
+
+
+        /// <summary>
+        /// Creates a new <see cref="DbContext"/> of <see cref="DomProjectContext"/> with custom settings.
+        /// </summary>
+        /// <param name="options"><see cref="DbContext"/> options.</param>
+        public DomProjectContext(DbContextOptions<DomProjectContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Question>()
+                .HasMany(x => x.AnswerValues)
+                .WithOne(x => x.Question);
+        }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<AnswerValue> AnswerValues { get; set; }
+
+    }
+}
