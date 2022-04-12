5482845ca94599d3f5869f6e3ca9c310ac07b61c
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-12 16:42:58 -0300

    Criação da classe de usuários.

---
+++ dom_project/dom_api/DomProject_WebApi/Dtos/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DomProject_WebApi.Models;
+
+namespace DomProject_WebApi.Dtos
+{
+    public class UserViewModel : DbObject
+    {
+        public string Email { get; set; }
+        public string Ssn { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ dom_project/dom_api/DomProject_WebApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DomProject_WebApi.Models
+{
+    public class User : DbObject
+    {
+        public string Email { get; set; }
+        public string Ssn { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
