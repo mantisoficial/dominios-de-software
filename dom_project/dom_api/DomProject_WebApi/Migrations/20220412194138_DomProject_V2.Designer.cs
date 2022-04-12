64958eb124ee3099c9933639d67849183181ef4d
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-12 16:43:41 -0300

    Segunda migração.

--- dom_project/dom_api/DomProject_WebApi/Migrations/20220412194138_DomProject_V2.Designer.cs
+++ dom_project/dom_api/DomProject_WebApi/Migrations/20220412194138_DomProject_V2.Designer.cs
@@ -3,14 +3,16 @@ using DomProject_WebApi.Repository.Sql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DomProject_WebApi.Migrations
 {
     [DbContext(typeof(DomProjectContext))]
-    partial class DomProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20220412194138_DomProject_V2")]
+    partial class DomProject_V2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
