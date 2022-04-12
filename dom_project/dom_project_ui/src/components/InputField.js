82e2d9dc4436a9be319a88101e9733d1a84243ca
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2022-04-12 18:26:54 -0300

    Nova propriedade para InputField.

--- dom_project/dom_project_ui/src/components/InputField.js
+++ dom_project/dom_project_ui/src/components/InputField.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./InputField.css";
 
-const InputField = ({ name, onChange, value, placeholder, inputStyle, inputLength}) => {
+const InputField = ({
+  name,
+  inputType,
+  onChange,
+  value,
+  placeholder,
+  inputStyle,
+  inputLength,
+}) => {
   return (
     <input
       name={name}
       className="text-input"
-      type="text"
+      type={inputType ? inputType : "text"}
       placeholder={placeholder ? placeholder : null}
       onChange={onChange}
       value={value}
