361c9dfb3881948d681639ef8069a5c2a77818d3
pokz <pietronieroboss@gmail.com>
2022-04-12 22:38:06 -0300

    Conexão da Dialog com a API para cadastro de questões.

--- dom_project/dom_project_ui/src/components/Dialog.js
+++ dom_project/dom_project_ui/src/components/Dialog.js
@@ -7,26 +7,45 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
   //#region UseState and Variables
   const [newQuestion, setNewQuestion] = useState({
     code: "",
-    name: "",
-    question: "",
-    subject: "Matemática",
-    subjectType: "Exact",
+    title: "",
+    text: "",
+    subject: "",
+    subjectType: "",
+    answerValues: [
+      {
+        text: "",
+        isCorrect: false,
+      },
+      {
+        text: "",
+        isCorrect: false,
+      },
+      {
+        text: "",
+        isCorrect: false,
+      },
+      {
+        text: "",
+        isCorrect: false,
+      },
+      {
+        text: "",
+        isCorrect: false,
+      },
+    ],
   });
 
   const [errorMessage, setErrorMessage] = useState("");
 
   const subjectOptions = [
     {
-      name: "Matemática",
+      name: "Exatas",
     },
     {
-      name: "História",
+      name: "Humanas",
     },
     {
-      name: "Física",
-    },
-    {
-      name: "Geografia",
+      name: "Biológicas",
     },
   ];
   //#endregion
@@ -34,37 +53,62 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
   //#region Handles
   const handleInputChange = (e) => {
     setNewQuestion({ ...newQuestion, [e.target.name]: e.target.value });
+    console.log(newQuestion);
   };
 
-  const handleCheckboxChange = () => {
-    if (newQuestion.reviewed === false)
-      setNewQuestion({ ...newQuestion, reviewed: true });
-    else setNewQuestion({ ...newQuestion, reviewed: false });
+  const handleAnswerInputChange = (index, e, isCheckbox) => {
+    if (!isCheckbox) {
+      setNewQuestion({
+        ...newQuestion,
+        answerValues: newQuestion.answerValues.map((item, itemIndex) => {
+          if (itemIndex === index) {
+            item.text = e.target.value;
+          }
+          return item;
+        }),
+      });
+    } else {
+      setNewQuestion({
+        ...newQuestion,
+        answerValues: newQuestion.answerValues.map((item, itemIndex) => {
+          if (itemIndex === index) {
+            if (item.isCorrect === false) item.isCorrect = true;
+            else if (item.isCorrect === true) item.isCorrect = false;
+          }
+          return item;
+        }),
+      });
+    }
   };
 
   const handleComboBoxChange = (e) => {
-    if (e.target.value === "Geografia" || e.target.value === "História") {
+    if (e.target.value === "Exatas") {
       setNewQuestion({
         ...newQuestion,
-        subject: e.target.value,
-        subjectType: "Human",
+        subject: 1,
+        subjectType: "Exatas",
       });
-    } else {
-      if (e.target.value === "Física" || e.target.value === "Matemática") {
-        setNewQuestion({
-          ...newQuestion,
-          subject: e.target.value,
-          subjectType: "Exact",
-        });
-      }
+    } else if (e.target.value === "Humanas") {
+      setNewQuestion({
+        ...newQuestion,
+        subject: 2,
+        subjectType: "Humanas",
+      });
+    } else if (e.target.value === "Biológicas") {
+      setNewQuestion({
+        ...newQuestion,
+        subject: 3,
+        subjectType: "Biológicas",
+      });
     }
+    console.log(newQuestion);
   };
 
   const handleValidation = () => {
     var isValid = true;
 
     existingQuestions.map((item) => {
-      if (newQuestion.question === item.question) {
+      if (newQuestion.text === item.text) {
         isValid = false;
         setErrorMessage("Questões iguais não são permitidas");
         return isValid;
@@ -72,11 +116,19 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
       return isValid;
     });
 
-    if (!newQuestion.code || !newQuestion.question) {
+    if (!newQuestion.code || !newQuestion.text || !newQuestion.title) {
       isValid = false;
       setErrorMessage("Preencha o formulário corretamente");
       return isValid;
     }
+
+    newQuestion.answerValues.map((item, index) => {
+      if (!item.text) {
+        isValid = false;
+        setErrorMessage("Preencha o formulário corretamente");
+        return isValid;
+      }
+    });
 
     return isValid;
   };
@@ -106,8 +158,7 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
           <div className="input-row">
             <div style={{ margin: "auto 0px" }}>
               <p className="body" style={{ margin: "0px 8px 0px 0px" }}>
-                {" "}
-                Código:{" "}
+                Código:
               </p>
             </div>
             <InputField
@@ -122,13 +173,12 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
           <div className="input-row">
             <div style={{ margin: "auto 0px" }}>
               <p className="body" style={{ margin: "0px 16px 0px 0px" }}>
-                {" "}
-                nome:{" "}
+                Título:
               </p>
             </div>
             <InputField
-              name="name"
-              value={newQuestion.name}
+              name="title"
+              value={newQuestion.title}
               placeholder="Preencha o campo"
               inputStyle={{ width: "200px" }}
               onChange={handleInputChange}
@@ -137,30 +187,59 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
           <div className="input-row" style={{ display: "block" }}>
             <div style={{ margin: "auto 0px" }}>
               <p className="body" style={{ margin: "0px 1px 4px 0px" }}>
-                {" "}
-                Questão:{" "}
+                Enunciado:
               </p>
             </div>
             <InputField
               inputStyle={{ width: "250px", heigth: "30px" }}
               placeholder="Preencha o campo"
-              name="question"
-              value={newQuestion.question}
+              name="text"
+              value={newQuestion.text}
               onChange={handleInputChange}
             />
+          </div>
+          <div className="input-row-answers">
+            {newQuestion.answerValues.map((item, index) => {
+              return (
+                <div className="answer-row" key={index}>
+                  <div style={{ margin: "auto 0px" }}>
+                    <p className="body" style={{ margin: "0px 16px 0px 0px" }}>
+                      Opção {index + 1}:
+                    </p>
+                  </div>
+                  <InputField
+                    name="text"
+                    value={item.text}
+                    placeholder="Preencha o campo"
+                    inputStyle={{ width: "200px" }}
+                    onChange={(e) => {
+                      handleAnswerInputChange(index, e, false);
+                    }}
+                  />
+                  <input
+                    name="isCorrect"
+                    className="text-input"
+                    type="checkbox"
+                    onChange={(e) => {
+                      handleAnswerInputChange(index, e, true);
+                    }}
+                    value={item.isCorrect}
+                  />
+                </div>
+              );
+            })}
           </div>
           <div className="input-row">
             <div style={{ margin: "auto 0px" }}>
               <p className="body" style={{ margin: "0px 1px 0px 0px" }}>
-                {" "}
-                Matéria:{" "}
+                Matéria:
               </p>
             </div>
             <select
-              name="subject"
+              name="subjectType"
               className="text-input"
               onChange={handleComboBoxChange}
-              value={newQuestion.subject}
+              value={newQuestion.subjectType}
             >
               {subjectOptions.map((item, index) => {
                 return (
@@ -170,21 +249,6 @@ const Dialog = ({ isOpen, onCancel, onSuccessCallback, existingQuestions }) => {
                 );
               })}
             </select>
-          </div>
-          <div className="input-row">
-            <div style={{ margin: "auto 0px" }}>
-              <p className="body" style={{ margin: "0px 1px 0px 0px" }}>
-                {" "}
-                Revisado:{" "}
-              </p>
-            </div>
-            <input
-              name="reviewed"
-              className="text-input"
-              type="checkbox"
-              onChange={handleCheckboxChange}
-              value={newQuestion.reviewed}
-            ></input>
           </div>
         </div>
         <div className="dialog-error">
